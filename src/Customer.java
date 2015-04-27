9f71643fcbdc8e9987ff19371d8a765c6e6b8540
Dennis Latt <kontakt@latt.org>
2015-04-27 13:44:04 +0200

    renamed to htmlStatement
    - chenged logic for htmlStatement

--- src/Customer.java
+++ src/Customer.java
@@ -17,23 +17,21 @@ class Customer {
 		return name;
 	};
 
-	public String statement() {
+	public String htmlStatement() {
 		Enumeration rentals = _rentals.elements();
-		String result = "Rental Record for " + this.getName() + "\n";
-		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount"
-				+ "\n";
-
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(each.getCharge()) + "\n";
+			// show figures for each rental
+			result += each.getMovie().getTitle() + ": "
+					+ String.valueOf(each.getCharge()) + "<BR>\n";
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		result += "You earned "
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge())
+				+ "</EM><P>\n";
+		result += "On this rental you earned <EM>"
 				+ String.valueOf(getTotalFrequentRenterPoints())
-				+ " frequent renter points";
+				+ "</EM> frequent renter points<P>";
 		return result;
 	}
 
