9df4b5bb5fde155791d11ca9bd8882e1fe00cf85
Dennis Latt <kontakt@latt.org>
2015-04-27 13:26:28 +0200

    added frequentRenter method

--- src/Customer.java
+++ src/Customer.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 class Customer {
 	private String name;
-	private Vector rentals = new Vector();
+	private Vector _rentals = new Vector();
 
 	public Customer(String newname) {
 		name = newname;
 	};
 
 	public void addRental(Rental arg) {
-		rentals.addElement(arg);
+		_rentals.addElement(arg);
 	};
 
 	public String getName() {
@@ -20,28 +20,19 @@ class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Enumeration enum_rentals = rentals.elements();
+		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount"
 				+ "\n";
 
-		while (enum_rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) enum_rentals.nextElement();
-			// determine amounts for each line
-			thisAmount = each.getCharge();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
-					+ each.getDaysRented() + "\t" + String.valueOf(thisAmount)
-					+ "\n";
-			totalAmount += thisAmount;
-		}
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			frequentRenterPoints += each.getFrequentRenterPoints();
+			//show figures for this rental
+			result += "\t" + each.getMovie().getTitle()+ "\t" +
+			String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
+			}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
